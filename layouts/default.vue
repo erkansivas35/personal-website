4d145096c64a9ecc9611123c462da03c0e0fc254
Erkan Sivas <erkan.sivas35@gmail.com>
2020-05-20 11:50:49 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  background-color: rgba(223, 223, 223, 0.15);
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+.button--blue {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #2a77b5;
+  color: #2a77b5;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--blue:hover {
+  color: #fff;
+  background-color: #2a77b5;
+}
+
+.button--light-blue {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3ca1f2;
+  color: #3ca1f2;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--light-blue:hover {
+  color: #fff;
+  background-color: #3ca1f2;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <h1 class="title">Hello, my name is Erkan.</h1>
+
+      <h2 class="subtitle">I am a Front End Developer living in Istanbul, TR.</h2>
+
+      <div class="links">
+        <a href="https://github.com/erkansivas35"
+           target="_blank"
+           class="button--grey">
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/erkansivas35/"
+           target="_blank"
+           class="button--blue">
+          LinkedIn
+        </a>
+        <a href="https://twitter.com/erkansivas35"
+           target="_blank"
+           class="button--light-blue">
+          Twitter
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage'
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.content {
+  width: 100%;
+  padding: 48px 0;
+  background-color: rgba(235, 235, 235, 0.4);
+}
+.title {
+  font-family: sans-serif;
+  font-weight: 900;
+  color: #35495e;
+  letter-spacing: 1px;
+  font-weight: 300;
+  font-size: 4rem;
+  padding-bottom: 16px;
+}
+
+.subtitle {
+  font-family: sans-serif;
+  color: rgba(53, 73, 94, 0.9);
+  font-weight: 300;
+  font-size: 1.5rem;
+  letter-spacing: -0.5;
+  padding-bottom: 16px;
+}
+
+.links {
+  padding-top: 16px;
+}
+
+.links a {
+  margin-right: 8px;
+}
+
+.links a:last-child {
+  margin-right: 0;
+}
+
+.links a {
+  transition: 0.3s;
+}
+</style>
