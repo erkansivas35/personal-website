6553c63b396488525be31de1bebceebc519e37c8
Erkan Sivas <erkan.sivas35@gmail.com>
2024-05-19 10:15:32 +0300

    Create nuxt.config.ts

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+    "nitro": {
+        "output": {
+            dir: 'output',
+            serverDir: 'output/server',
+            publicDir: 'output/public'
+        }
+    }
+})
